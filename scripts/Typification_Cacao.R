6a5b5634e9dca30c576a73e7791d5ff443bc6bce
PAULA ANDREA BERMEO FÚQUENE <pbermeo@agrosavia.co>
2026-02-19 10:14:46 -0500

    Correlation matrix added

--- scripts/Typification_Cacao.R
+++ scripts/Typification_Cacao.R
@@ -1,4 +1,4 @@
-### Typification cacao code farms map####
+### Typification cacao code#### 
 
 pacman::p_load(dplyr, cartography, cowplot, geobr, ggplot2, ggpattern, ggspatial, gridGraphics, sf, sp, tmap, terra, raster, readr, rnaturalearth, viridis)
 
@@ -6,8 +6,9 @@ pacman::p_load(dplyr, cartography, cowplot, geobr, ggplot2, ggpattern, ggspatial
 DEM <- raster("Mapa fincas/elev.tif")
 
 #Load shapefile
-COL <- st_read("Mapa fincas/Servicios_Públicos_-_Municipios_2005.shp") 
+COL <- st_read("Mapa fincas/Servicios_Públicos_-_Municipios_2005.shp")
 SANT <- COL[COL$DEPTO == "SANTANDER", ]
+plot(SANT)
 
 #Load farms location
 Farms <- read_delim("Mapa fincas/Occs_fincas.csv", delim = ';', col_names = T) |> as.data.frame()
@@ -15,32 +16,32 @@ wgs <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 WGS84 <- sp::CRS(wgs)
 occ <- sp::SpatialPointsDataFrame(Farms[, c('LONGITUDE', 'LATITUDE')], Farms, proj4string = WGS84)
 
-# change to tmap mode
-tmap::tmap_mode(mode = "plot")
-tmap_options(check.and.fix = TRUE)
+#
 
-palette <- viridis_pal(begin = 0, end = 1, option = 'C', direction = -1)(10)
+tm_shape(DEM, bbox= c(-8296522, -8296522, -8068109, 909767.8)) +
+  tm_raster(style = "cont", legend.reverse= T, #legend.format = list(text.align = 'right'),
+            palette = palette, title = 'Elevation (m)') +
 
-#Map
-    tm_shape(DEM, bbox= c(-83, 63, -80.4, 8.2)) +
-      tm_raster(style = "cont", legend.reverse= T, #legend.format = list(text.align = 'right'),
-                    palette = palette, title = 'Elevation (m)') +
-    tm_shape(SANT) +
-      tm_borders(col = 'gray50', lwd = 0.5) +  
-    tm_shape(occ) +
-    tm_symbols(size= 0.2, col= 'red', alpha= 0.8) +
-    tm_compass(position = c(0.9, 0.7), size= 3.2) + 
-    tm_scale_bar(text.size = 0.6, position = c(0.4, 0), width = 0.2) + #scale bar
-    tm_graticules(lines = FALSE, labels.rot = c(0, 90), labels.size = 0.8) +
-    tm_add_legend(type = "symbol", 
-                  labels = "Cacao Farms", 
-                  col = "red", 
-                  lwd = 3,
-                  size = 0.5
-    ) +
-    tm_add_legend(type = "line", 
-                  labels = "Administrative border", 
-                  col = "gray50", 
-                  lwd = 3
-    ) 
-  Map <- grid.grab()
+  
+tm_shape(DEM, bbox= c(-83,-80.4, 63,8.2)) +
+  tm_raster(style = "cont", legend.reverse= T, #legend.format = list(text.align = 'right'),
+            palette = palette, title = 'Elevation (m)')tm_shape(DEM, bbox= c(-83, 63, -80.4, 8.2)) +
+  tm_shape(SANT) +
+  tm_borders(col = 'gray50', lwd = 0.5) +
+  tm_shape(occ) +
+  tm_symbols(size= 0.2, col= 'red', alpha= 0.8) +
+  tm_compass(position = c(0.9, 0.7), size= 3.2) +
+  tm_scale_bar(text.size = 0.6, position = c(0.4, 0), width = 0.2) + #scale bar
+  tm_graticules(lines = FALSE, labels.rot = c(0, 90), labels.size = 0.8)
+  tm_add_legend(type = "symbol",
+              labels = "Cacao Farms",
+              col = "red",
+              lwd = 3,
+              size = 0.5
+  ) +
+  tm_add_legend(type = "line",
+                labels = "Administrative border",
+                col = "gray50",
+                lwd = 3
+  )
+
